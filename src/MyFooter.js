a500430acd037c9ed6cbf1ca1e349fd1ca13cec8
61114440694 <sakkarin.ha.61@ubu.ac.th>
2023-06-17 11:25:27 +0700

    create first react app

---
+++ src/MyFooter.js
@@ -0,0 +1,11 @@
+import './App.css'
+function MyFooter() {
+    return (
+        <div className="My-footer">
+                <p>
+                    Copyright @ 2023 Sakkarin Harnchanaphol
+                </p>
+            </div>
+    )
+}
+export default MyFooter
