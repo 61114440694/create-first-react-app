3e9c127979b0d9d538bbf852c9036f733207c602
61114440694 <sakkarin.ha.61@ubu.ac.th>
2023-06-20 00:18:23 +0700

    Commit Code Assignment# 01

--- src/App.js
+++ src/App.js
@@ -7,14 +7,38 @@ function App() {
     setInterval(UpdateTime, 1000);
     let now = new Date().toLocaleTimeString('th-TH');
     const [time, setTime] = useState(now);
-    const [num , setNum] = useState(0);
+    const [num, setNum] = useState(0);
+    const [showtext, setShowText] = useState()
+
 
     function UpdateTime() {
         let NewTime = new Date().toLocaleTimeString('th-TH');
         setTime(NewTime)
+
     }
 
-    
+    function CheckNumToShow(x) {
+        let Prime = true
+        if (x === 1) {
+            setShowText(`${x} ไม่เป็นจำนวนเฉพาะเพราะมีตัวหารลงตัว 1 ตัว`)
+        } else if (x > 1) {
+            for (let i = 2; i < x; i++) {
+                if (x % i === 0) {
+                    Prime = false
+                    break;
+                }
+            }
+            if (Prime) {
+                setShowText(`${x} เป็นจำนวนเฉพาะ`)
+            }else{
+                setShowText(`${x} ไม่เป็นจำนวนเฉพาะ`)
+            }
+        } else {
+            setShowText(`${x}ไม่เป็นจำนวนเฉพาะ`)
+        }
+    }
+
+
     return (
         <div>
             <MyHeader/>
@@ -23,9 +47,21 @@ function App() {
                     ขณะนี้เวลา
                     <div>{time}</div>
                     <h2>{num}</h2>
-                    <button onClick={()=>setNum(num+1)}>เพิ่ม</button>
-                    <button onClick={()=>setNum(num-1)}>ลด</button>
-                    <p></p>
+                    <button onClick={
+                        () => {
+                            const x = num + 1
+                            setNum(x)
+                            CheckNumToShow(x)
+                        }
+                    }>เพิ่ม</button>
+                    <button onClick={
+                        () => {
+                            const y = num - 1
+                            setNum(y)
+                            CheckNumToShow(y)
+                        }
+                    }>ลด</button>
+                    <p>{showtext}</p>
                 </div>
 
             </div>
